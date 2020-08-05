d7648312639f0f212e787016a6535b08a2eed360
Hector Pascual <hector.pascual.diaz@gmail.com>
2020-08-05 18:11:36 +0200

    Updates

---
+++ precache-manifest.1dedab13510e72a085fb355080ea31bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5af6f0d183de232ebb466e24946bd3",
+    "url": "/MusicQuiz/index.html"
+  },
+  {
+    "revision": "f3ae4fd163468f382ca9",
+    "url": "/MusicQuiz/static/js/2.f7181fa1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MusicQuiz/static/js/2.f7181fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe6e516ed1154193d7e",
+    "url": "/MusicQuiz/static/js/main.9f8296bd.chunk.js"
+  },
+  {
+    "revision": "f4cef7c1c3561cde5a6f",
+    "url": "/MusicQuiz/static/js/runtime-main.bcfc4c2c.js"
+  },
+  {
+    "revision": "59db3b912dae2784bae6c99e1fa42a41",
+    "url": "/MusicQuiz/static/media/vinyls.59db3b91.jpg"
+  }
+]);
